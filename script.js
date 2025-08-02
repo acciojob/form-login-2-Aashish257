0055d0440837434b7075f5d7d684749801a5e97d
Aashish Shilkande <83578134+Aashish257@users.noreply.github.com>
2025-08-02 14:15:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function details() {
+	let firstname = document.getElementById("first_name").value;
+	let lastname = document.getElementById("last_name").value;
+	let MobileNo = document.getElementById("phone_number").value;
+	let EmailID = document.getElementById("Email_id").value;
+
+	alert(`First Name: ${firstname} Last Name: ${lastname} Phone Number: ${MobileNo} Email ID: ${EmailID}`);
+}
